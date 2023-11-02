08bae564978543803f549c431cbd691c995fb846
toyota-m2k <toyota.mitsuki@gmail.com>
2023-11-02 21:41:59 +0900

    chore: targetSdk 33-->34, jvm 1.8-->17

--- app/src/main/java/io/github/toyota32k/sample/MainActivity.kt
+++ app/src/main/java/io/github/toyota32k/sample/MainActivity.kt
@@ -2,7 +2,7 @@ package io.github.toyota32k.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import io.github.toyota32k.utils.R
+import io.github.toyota32k.sample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
