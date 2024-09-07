bf1e68fe891112cb5fd71201875756bcb8625302
toyota-m2k <toyota.mitsuki@gmail.com>
2024-09-07 19:44:17 +0900

    feat: ColorExt

---
+++ libUtils/src/main/java/io/github/toyota32k/utils/ColorExt.kt
@@ -0,0 +1,79 @@
+package io.github.toyota32k.utils
+
+import android.graphics.Color
+import androidx.annotation.ColorInt
+
+data class RGB(val r:Int, val g:Int, val b:Int, val a:Int) {
+    constructor(@ColorInt rgba: Int) : this(Color.red(rgba), Color.green(rgba), Color.blue(rgba), Color.alpha(rgba))
+    constructor(rgba: Long) : this(rgba.toInt())
+
+    @ColorInt
+    fun toColor() : Int {
+        return Color.argb(a, r, g, b)
+    }
+
+    fun brend(other: RGB) : RGB {
+        return RGB(
+            (r + other.r) / 2,
+            (g + other.g) / 2,
+            (b + other.b) / 2,
+            255
+        )
+    }
+
+    companion object {
+        fun opaque(@ColorInt rgba: Int) : RGB {
+            return RGB(Color.red(rgba), Color.green(rgba), Color.blue(rgba), 255)
+        }
+        fun opaque(rgba: Long) : RGB {
+            return opaque(rgba.toInt())
+        }
+
+        val black = RGB(0, 0, 0, 255)
+        val white = RGB(255, 255, 255, 255)
+
+        @ColorInt
+        fun darken(@ColorInt rgba:Int) : Int {
+            return RGB(rgba).brend(black).toColor()
+        }
+        @ColorInt
+        fun darken(rgba:Long) : Int {
+            return RGB(rgba).brend(black).toColor()
+        }
+        @ColorInt
+        fun lighten(@ColorInt rgba:Int) : Int {
+            return RGB(rgba).brend(white).toColor()
+        }
+        @ColorInt
+        fun lighten(rgba:Long) : Int {
+            return RGB(rgba).brend(white).toColor()
+        }
+        @ColorInt
+        fun brend(@ColorInt rgba1:Int, @ColorInt rgba2:Int) : Int {
+            return RGB(rgba1).brend(RGB(rgba2)).toColor()
+        }
+        @ColorInt
+        fun brend(rgba1:Long, rgba2:Long) : Int {
+            return RGB(rgba1).brend(RGB(rgba2)).toColor()
+        }
+    }
+}
+
+/**
+ * ColorIntにアルファ値を付与
+ */
+@ColorInt
+fun @receiver:ColorInt Int.withAlpha(alpha:Int) : Int  {
+    return this and 0x00ffffff or (alpha shl 24)
+}
+
+@ColorInt
+fun Long.colorWithAlpha(alpha:Int) : Int {
+    return toInt() and 0x00ffffff or (alpha shl 24)
+}
+
+@ColorInt
+fun Int.opaqueColor() : Int {
+    return this or 0xff000000.toInt()
+}
+
