ecad6a8b9ec04b1783fd7b69da64f100b34ab4a1
toyota-m2k <toyota.mitsuki@gmail.com>
2025-01-12 08:57:12 +0900

    feat: UtScaleGestureManager

---
+++ libUtils/src/main/java/io/github/toyota32k/utils/gesture/UtAbstractManipulationTarget.kt
@@ -0,0 +1,29 @@
+package io.github.toyota32k.lib.player.gesture
+
+import io.github.toyota32k.shared.gesture.Direction
+import io.github.toyota32k.shared.gesture.IUtManipulationTarget
+import io.github.toyota32k.shared.gesture.Orientation
+import java.util.EnumSet
+
+/**
+ * 最小限（ページングなし）のIUtManipulationTargetの実装
+ */
+abstract class UtAbstractManipulationTarget : IUtManipulationTarget {
+    override val overScrollX: Float = 0f
+    override val overScrollY: Float = 0f
+    override val pageOrientation: EnumSet<Orientation> = EnumSet.noneOf(Orientation::class.java)
+
+    override fun changePage(
+        orientation: Orientation,
+        dir: Direction
+    ): Boolean {
+        return false
+    }
+
+    override fun hasNextPage(
+        orientation: Orientation,
+        dir: Direction
+    ): Boolean {
+        return false
+    }
+}
---
+++ libUtils/src/main/java/io/github/toyota32k/utils/gesture/UtScaleGestureManager.kt
@@ -0,0 +1,50 @@
+package io.github.toyota32k.lib.player.gesture
+
+import android.content.Context
+import android.view.View
+import androidx.lifecycle.LifecycleOwner
+import io.github.toyota32k.shared.gesture.IUtManipulationTarget
+import io.github.toyota32k.shared.gesture.UtGestureInterpreter
+import io.github.toyota32k.shared.gesture.UtGestureInterpreter.IListenerBuilder
+import io.github.toyota32k.shared.gesture.UtManipulationAgent
+
+/**
+ * ピンチによるズーム操作を簡単に実現するために、
+ * ジェスチャー（UtGestureInterpreter）、対象ビュー（IUtManipulationTarget）、アクション(UtManipulationAgent) をまとめて管理するクラス
+ *
+ * usage
+ * Activity
+ *   private lateinit var scaleGestureManager: UtScaleGestureManager
+ *   override fun onCreate(savedInstanceState: Bundle?) {
+ *      ...
+ *      scaleGestureManager = UtScaleGestureManager(this.applicationContext, true).setup(this) {
+ *          onTap {
+ *              ...
+ *          }
+ *          onDoubleTap {
+ *              ...
+ *          }
+ *          ...
+ *      }
+ *   }
+ */
+class UtScaleGestureManager(
+    applicationContext: Context,
+    enableDoubleTap:Boolean,        // !rapidTap for GestureInterpreter
+    val manipulationTarget: IUtManipulationTarget,
+    minScale:Float = 0f, maxScale:Float = 10f,
+) {
+    val gestureInterpreter = UtGestureInterpreter(applicationContext, true, !enableDoubleTap)
+    val agent = UtManipulationAgent(manipulationTarget, minScale, maxScale)
+
+    /**
+     * Activity#onCreate()から呼び出す
+     */
+    @Suppress("unused")
+    fun setup(owner:LifecycleOwner, view:View? = null, setupMe: IListenerBuilder.()->Unit) : UtScaleGestureManager {
+        gestureInterpreter.setup(owner, view ?: manipulationTarget.parentView, setupMe)
+        gestureInterpreter.scrollListener.add(owner, agent::onScroll)
+        gestureInterpreter.scaleListener.add(owner, agent::onScale)
+        return this
+    }
+}
