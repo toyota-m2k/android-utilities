1225d5060678844688b5d1e2fc98053ad552c7fb
toyota-m2k <toyota.mitsuki@gmail.com>
2025-07-18 20:33:43 +0900

    feat: WorkerDataDelegate / WorkerParams

---
+++ libUtils/src/main/java/io/github/toyota32k/utils/android/WorkerParams.kt
@@ -0,0 +1,22 @@
+package io.github.toyota32k.utils.android
+
+import androidx.work.Data
+
+/**
+ * タイプセーフな androidx.work.Data の作成(produce)と読み取り（consume) をサポートするクラス。
+ * - コンストラクタに null を渡すと、Data作成用に初期化される。プロパティにデータをセットして produce()を呼び出すことで Dataインスタンスが作成される。
+ * - コンストラクタに データインスタンス（inputData）を渡すと、inputDataからの読み出し用に初期化される（Worker内で、パラメータを取り出すために利用する）
+ *
+ */
+abstract class WorkerParams(inputData: Data?) {
+    protected val delegate = WorkerDataDelegate(inputData)
+    val forConsumer get():Boolean = delegate.data != null
+    val forProducer get():Boolean = delegate.data == null
+
+//    val hoge:Int by delegate.intZero
+
+    fun produce():Data {
+        assert(forProducer)
+        return delegate.composeData()
+    }
+}
