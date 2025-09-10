26b694fdfaef6d62b10b3a48c2917c0a62bf2e15
toyota-m2k <toyota.mitsuki@gmail.com>
2025-09-10 16:39:10 +0900

    fix: TimeSpan milliseconds format

--- libUtils/src/main/java/io/github/toyota32k/utils/TimeSpan.kt
+++ libUtils/src/main/java/io/github/toyota32k/utils/TimeSpan.kt
@@ -4,16 +4,19 @@ import java.util.Locale
 import kotlin.time.Duration
 
 class TimeSpan (ms:Long) {
-    val milliseconds: Long = ms
+    val totalMilliseconds: Long = ms
 
     val seconds: Long
-        get() = (milliseconds / 1000) % 60
+        get() = (totalMilliseconds / 1000) % 60
 
     val minutes: Long
-        get() = (milliseconds / 1000 / 60) % 60
+        get() = (totalMilliseconds / 1000 / 60) % 60
 
     val hours: Long
-        get() = (milliseconds / 1000 / 60 / 60)
+        get() = (totalMilliseconds / 1000 / 60 / 60)
+
+    val milliseconds: Long
+        get() = totalMilliseconds % 1000
 
     fun formatH(format:String="%02d:%02d'%02d\"") : String {
         return String.format(Locale.US, format, hours, minutes, seconds)
@@ -21,13 +24,13 @@ class TimeSpan (ms:Long) {
     fun formatM(format:String="%02d'%02d\"") : String {
         return String.format(Locale.US, format, minutes, seconds)
     }
-    fun formatMm(format:String="%02d'%02d\"%03d") : String {
+    fun formatMm(format:String="%02d'%02d\" %03d") : String {
         return String.format(Locale.US, format, minutes, seconds, milliseconds)
     }
-    fun formatS(format:String="%02d\"%02d") : String {
+    fun formatS(format:String="%02d\" %02d") : String {
         return String.format(Locale.US, format, seconds, milliseconds/10)
     }
-    fun formatSm(format:String="%02d\"%03d") : String {
+    fun formatSm(format:String="%02d\" %03d") : String {
         return String.format(Locale.US, format, seconds, milliseconds)
     }
     fun formatAuto() : String {
