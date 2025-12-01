fa30c62a4267ff42c47e13f938bfcde3d5da273b
toyota-m2k <toyota.mitsuki@gmail.com>
2025-12-01 14:08:30 +0900

    fix: critical initialization probrem of UtMutableStateFlowLiveData

--- libUtils/src/main/java/io/github/toyota32k/utils/lifecycle/UtMutableStateFlowLiveData.kt
+++ libUtils/src/main/java/io/github/toyota32k/utils/lifecycle/UtMutableStateFlowLiveData.kt
@@ -1,6 +1,8 @@
 package io.github.toyota32k.utils.lifecycle
 
 import androidx.lifecycle.*
+import io.github.toyota32k.utils.FlowableEvent
+import io.github.toyota32k.utils.UtLib.logger
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
@@ -9,8 +11,10 @@ import kotlinx.coroutines.launch
  * 通常は、MutableStateFlow.asMutableLiveData() を使って構築する。
  */
 class UtMutableStateFlowLiveData<T>(val flow: MutableStateFlow<T>, lifecycleOwner: LifecycleOwner?=null): MutableLiveData<T>(), Observer<T> {
+    var prevLiveDataValue:T? = null
     init {
         value = flow.value
+        prevLiveDataValue = flow.value
         if(null!=lifecycleOwner) {
             attachToLifecycle(lifecycleOwner)
         }
@@ -22,21 +26,25 @@ class UtMutableStateFlowLiveData<T>(val flow: MutableStateFlow<T>, lifecycleOwne
         lifecycleOwner.lifecycleScope.launch {
             lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 flow.collect {
-                    postValue(it)
+                    if (value!==it) {
+                        value = it
+                    }
                 }
             }
         }
-//        lifecycleOwner.lifecycleScope.launchWhenStarted {
-//            flow.collect {
-//                if(value!==it) {
-//                    value = it
-//                }
-//            }
-//        }
     }
 
     override fun onChanged(value: T) {
-        flow.value = value
+        // 値が変化していない場合の無駄打ちを止める
+        // 初期化(init->attachToLifecycle)直後の（MutableStateFlow側の）値変更が反映されない現象が発生。
+        // LiveData#observe(lifecycleOwner, this) のあと、必ず初回の onChanged が呼ばれるが、
+        // これが、flow.collect() による MutableStateFlow の値変更監視開始後に呼ばれるため、
+        // flowに対する値変更が、onChangedによって、初期値（＝observe呼び出し時点のLiveData#value）で上書きされてしまう。
+        // これを回避するため、liveDataの前回値と比較して、変更がない場合は flow.value への反映を行わないよう修正した。
+        if (prevLiveDataValue!=value) {
+            prevLiveDataValue = value
+            flow.value = value
+        }
     }
 }
 
